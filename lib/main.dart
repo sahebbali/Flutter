3c44b45644b1d7a759e449aa581e13f85fca475e
sahebbali <saheb1996jmr@gmail.com>
2025-06-20 09:13:30 +0600

    add dispaly widgets

--- lib/main.dart
+++ lib/main.dart
@@ -6,55 +6,107 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: InkWellExample(),
+      title: 'Display Widgets Example',
+      home: DisplayWidgetsScreen(),
     );
   }
 }
 
-class InkWellExample extends StatefulWidget {
+class DisplayWidgetsScreen extends StatefulWidget {
   @override
-  _InkWellExampleState createState() => _InkWellExampleState();
+  _DisplayWidgetsScreenState createState() => _DisplayWidgetsScreenState();
 }
 
-class _InkWellExampleState extends State<InkWellExample> {
-  String _message = "Tap the box";
+class _DisplayWidgetsScreenState extends State<DisplayWidgetsScreen> {
+  String message = "Tap something";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("InkWell Example")),
-      body: Center(
-        child: InkWell(
-          onTap: () {
-            setState(() {
-              _message = "Box Tapped!";
-            });
-          },
-          onDoubleTap: () {
-            setState(() {
-              _message = "Double Tapped!";
-            });
-          },
-          onLongPress: () {
-            setState(() {
-              _message = "Long Pressed!";
-            });
-          },
-          child: Container(
-            width: 200,
-            height: 200,
-            decoration: BoxDecoration(
-              color: Colors.lightBlue,
-              borderRadius: BorderRadius.circular(12),
+      appBar: AppBar(title: Text('Display Widgets')),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+
+            /// Stack Example
+            Text(
+              '🔲 Stack Widget',
+              style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            alignment: Alignment.center,
-            child: Text(
-              _message,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(width: 150, height: 150, color: Colors.blue),
+                Container(width: 100, height: 100, color: Colors.green),
+                Text(
+                  "Top",
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ],
             ),
-          ),
+
+            SizedBox(height: 30),
+
+            /// Wrap Example
+            Text(
+              '🔁 Wrap Widget',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Wrap(
+              spacing: 10,
+              runSpacing: 10,
+              children: List.generate(8, (index) {
+                return Chip(label: Text('Item $index'));
+              }),
+            ),
+
+            SizedBox(height: 30),
+
+            /// GestureDetector Example
+            Text(
+              '👆 GestureDetector',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            GestureDetector(
+              onTap: () => setState(() => message = "GestureDetector Tapped!"),
+              onDoubleTap: () => setState(() => message = "Double Tapped!"),
+              onLongPress: () => setState(() => message = "Long Pressed!"),
+              child: Container(
+                margin: EdgeInsets.all(10),
+                width: 200,
+                height: 100,
+                color: Colors.amber,
+                alignment: Alignment.center,
+                child: Text(message, textAlign: TextAlign.center),
+              ),
+            ),
+
+            SizedBox(height: 30),
+
+            /// InkWell Example
+            Text('💦 InkWell', style: TextStyle(fontWeight: FontWeight.bold)),
+            Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () => setState(() => message = "InkWell Tapped!"),
+                borderRadius: BorderRadius.circular(12),
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.teal,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Text(
+                    'Tap Me (InkWell)',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 50),
+          ],
         ),
       ),
     );
